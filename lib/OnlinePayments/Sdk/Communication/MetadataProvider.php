daae4fc932f01e94b09e4d7eca3e940181c24dc2
Online Payments support team <96182451+online-payments-support-team@users.noreply.github.com>
2025-10-15 09:44:54 +0200

    Release version 7.3.1

--- lib/OnlinePayments/Sdk/Communication/MetadataProvider.php
+++ lib/OnlinePayments/Sdk/Communication/MetadataProvider.php
@@ -12,7 +12,7 @@ use OnlinePayments\Sdk\Domain\ShoppingCartExtension;
  */
 class MetadataProvider implements MetadataProviderInterface
 {
-    const SDK_VERSION = '7.3.0';
+    const SDK_VERSION = '1.0.0';
 
     /** @var string */
     private string $integrator;
